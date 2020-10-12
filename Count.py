b4df54f688baac02f023d5100440d365226a1cc5
G00387859 <G00387859@gmit.ie>
2020-10-12 21:39:48 +0100

    Adding additonal references to the code.

--- Count.py
+++ Count.py
@@ -1,10 +1,12 @@
 """
 References : 1. https://www.geeksforgeeks.org/python-frequency-of-each-character-in-string/
+             2. https://www.w3schools.com/python/python_dictionaries.asp
+             3. https://www.geeksforgeeks.org/python-convert-case-of-elements-in-a-list-of-strings/
 """
 def count(list1):
 # Converting list to upper case
     l = [x.upper() for x in list1]
-# create a empty dict    
+# create a empty dict [2]   
     items = {}
 #loop over the list
     for i in l:
